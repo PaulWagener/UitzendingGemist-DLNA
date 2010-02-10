3aac5db4898ba1cb750f7749a05e72603987bf0c
Paul Wagener <mail@paulwagener.nl>
2010-02-10 12:42:31 +0000

    ProgrammaGemist toegevoegd & maakt nu gebruik van AdditionalRootDot Code niet getest Plugin nu enkel compatible met PMS r387+

--- src/net/pms/uitzendinggemist/ProgrammaGemist.java
+++ src/net/pms/uitzendinggemist/ProgrammaGemist.java
@@ -2,12 +2,18 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package net.pms.uitzendinggemist;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import net.pms.configuration.RendererConfiguration;
+import net.pms.dlna.WebStream;
 import net.pms.dlna.virtual.VirtualFolder;
+import net.pms.formats.Format;
 import net.pms.uitzendinggemist.web.HTTPWrapper;
 
 /**
@@ -15,28 +21,70 @@ import net.pms.uitzendinggemist.web.HTTPWrapper;
  * @author paulwagener
  */
 public class ProgrammaGemist extends VirtualFolder {
-    public ProgrammaGemist()
-    {
 
-        super("SBS", null);
+    public final static String SBS6_URL = "http://www.sbs6.nl/web/show/id=73863/langid=43";
+    public final static String NET5_URL = "http://www.net5.nl/web/show/id=95681/langid=43";
+    public final static String VERONICA_URL = "http://www.veronicatv.nl/web/show/id=96520/langid=43";
+
+    public ProgrammaGemist(String naam, String URL) {
+        this(naam, URL, false);
+    }
+    String naam;
+    String url;
+    boolean losseAfleveringen;
+
+    public ProgrammaGemist(String naam, String URL, boolean losseAfleveringen) {
+        super(naam, null);
+        this.url = URL;
+        this.losseAfleveringen = losseAfleveringen;
     }
 
-    public static void main(String args[])
-    {
-        String programmaPagina = HTTPWrapper.Request("http://www.veronicatv.nl/web/show/id=113774/langid=43");
+    @Override
+    public void discoverChildren() {
+        super.discoverChildren();
+
+        String programmaPagina = HTTPWrapper.Request(url);
 
         //Filter op overzicht
         Matcher m1 = Pattern.compile("(?s)(Programma gemist overzicht|Bekijk alle afleveringen).*?class=\"bottom\"").matcher(programmaPagina);
-        if(m1.find())
+        if (m1.find()) {
             programmaPagina = m1.group();
+        }
 
         //Zoek shows
         Matcher m = Pattern.compile("(?s)class=\"thumb\".*?href=\"(/web/show.*?)\".*?src=\"(.*?)\".*?<span>(.*?)</span>").matcher(programmaPagina);
 
-        //TODO: iets DOEN met de data
         while (m.find()) {
-            System.out.println(m.group(3) + " " + m.group(1) + " " + m.group(2));
+            if (losseAfleveringen) {
+                addChild(new ProgrammaStream(m.group(3), m.group(1), m.group(2)));
+            } else {
+                addChild(new ProgrammaGemist(m.group(3), m.group(1), true));
+            }
+
         }
     }
 
+    static class ProgrammaStream extends WebStream {
+        String locatieUrl; //Webpagina waar filmpje te bekijken is
+        String echteUrl = null; //URL van wmv file
+
+        public ProgrammaStream(String naam, String url, String img) {
+            super(naam, "mms://url.invalid/url", img, Format.VIDEO);
+            this.locatieUrl = url;
+        }        
+
+        @Override
+        public InputStream getInputStream(long low, long high, double timeseek, RendererConfiguration mediarenderer) throws IOException {
+            if (echteUrl == null) {
+                String pagina = HTTPWrapper.Request("http://www.sbs6.nl" + locatieUrl);
+
+                Matcher m = Pattern.compile("(?s)class=\"wmv-player-holder\" href=\"(.*?))\"").matcher(pagina);
+                m.find();
+
+                this.URL = echteUrl = m.group();
+            }
+
+            return super.getInputStream(low, high, timeseek, mediarenderer);
+        }
+    }
 }
--- src/net/pms/uitzendinggemist/UitzendingGemist.java
+++ src/net/pms/uitzendinggemist/UitzendingGemist.java
@@ -2,102 +2,31 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package net.pms.uitzendinggemist;
 
 import javax.swing.JComponent;
-import net.pms.configuration.RendererConfiguration;
+import net.pms.dlna.DLNAResource;
 import net.pms.dlna.virtual.VirtualFolder;
-import net.pms.external.ExternalListener;
+import net.pms.external.AdditionalFolderAtRoot;
 
 /**
  *
- * @author Paul Wagener
+ * @author paulwagener
  */
-public class UitzendingGemist implements ExternalListener {
+public class UitzendingGemist implements AdditionalFolderAtRoot {
 
-    private static final String DAG_URL = "http://www.uitzendinggemist.nl/index.php/selectie?searchitem=dag&dag=";
-    private static final String TITEL_URL = "http://www.uitzendinggemist.nl/index.php/selectie?searchitem=titel&titel=";
-    private static final String SELECTIE_URL = "http://www.uitzendinggemist.nl/index.php/selectie";
-    //private static final String
-
-    public UitzendingGemist() {
+    public DLNAResource getChild() {
         VirtualFolder mainFolder = new VirtualFolder("Uitzending Gemist", null);
-
-        VirtualFolder dezeweekFolder = new VirtualFolder("Deze week", null);
-        dezeweekFolder.addChild(new UitzendingFolder("Vandaag", DAG_URL + "vandaag"));
-        dezeweekFolder.addChild(new UitzendingFolder("Gisteren", DAG_URL + "gisteren"));
-        dezeweekFolder.addChild(new UitzendingFolder("Maandag", DAG_URL + "1"));
-        dezeweekFolder.addChild(new UitzendingFolder("Dinsdag", DAG_URL + "2"));
-        dezeweekFolder.addChild(new UitzendingFolder("Woensdag", DAG_URL + "3"));
-        dezeweekFolder.addChild(new UitzendingFolder("Donderdag", DAG_URL + "4"));
-        dezeweekFolder.addChild(new UitzendingFolder("Vrijdag", DAG_URL + "5"));
-        dezeweekFolder.addChild(new UitzendingFolder("Zaterdag", DAG_URL + "6"));
-        dezeweekFolder.addChild(new UitzendingFolder("Zondag", DAG_URL + "7"));
-
-        //Op titel
-        VirtualFolder titelFolder = new VirtualFolder("Op titel", null);
-
-        String alfabet = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        for (int i = 0; i < alfabet.length(); i++) {
-            UitzendingFolder folder = new UitzendingFolder(alfabet.charAt(i) == '#' ? "0-9" : "" + alfabet.charAt(i), TITEL_URL + i, "items=actueel");
-            folder.addChild(new UitzendingFolder("Alle programma's", TITEL_URL + i));
-            titelFolder.addChild(folder);
-        }
-
-        //Op genre
-        final String[][] genres = {{"1", "Amusement"}, {"2", "Animatie"}, {"3", "Comedy"}, {"4", "Documentaire"},
-            {"21", "Drama"}, {"24", "Educatief"}, {"5", "Erotiek"}, {"6", "Film"},
-            {"27", "Gezondheid"}, {"7", "Informatief"}, {"8", "Jeugd"}, {"25", "Kinderen 2-5"},
-            {"26", "Kinderen 6-12"}, {"23", "Klassiek"}, {"9", "Kunst/Cultuur"}, {"19", "Maatschappij"},
-            {"10", "Misdaad"}, {"11", "Muziek"}, {"12", "Natuur"}, {"13", "Nieuws/actualiteiten"},
-            {"14", "Overige"}, {"15", "Religieus"}, {"16", "Serie/soap"}, {"17", "Sport"}, {"18", "Wetenschap"}};
-
-        VirtualFolder genreFolder = new VirtualFolder("Op genre", null);
-        for (int i = 0; i < genres.length; i++) {
-            final String url = SELECTIE_URL + "?searchitem=genre&genre=" + genres[i][0];
-            UitzendingFolder folder = new UitzendingFolder(genres[i][1], url, "items=actueel");
-            folder.addChild(new UitzendingFolder("Alle programma's", url));
-            genreFolder.addChild(folder);
-        }
-
-        //Op omroep
-        final String[][] omroepen = {
-            {"33", "3FM"}, {"11", "AVRO"}, {"16", "BNN"}, {"7", "BOS"}, {"15", "EO"}, {"29", "HUMAN"}, {"3", "IKON"}, {"48", "Joodse Omroep"}, {"2", "KRO"},
-            {"45", "LLiNK"}, {"46", "MAX"}, {"52", "MTNL"}, {"8", "NCRV"}, {"55", "Nederland 1"}, {"56", "Nederland 2"},
-            {"21", "Nederland 3"}, {"49", "NIO"}, {"4", "NMO"}, {"12", "NOS"}, {"22", "NPS"}, {"5", "OHM"},
-            {"50", "Omroep-nl"}, {"6", "OMROP FRYSLAN"}, {"32", "Radio 2"}, {"27", "Radio 4"}, {"1", "RKK"}, {"25", "RNW"}, {"23", "RVU"}, {"43", "TELEAC"},
-            {"14", "TROS"}, {"17", "VARA"}, {"20", "VPRO"}, {"47", "Z@pp"}, {"19", "Z@ppelin"}, {"28", "ZvK"}};
-
-        VirtualFolder omroepFolder = new VirtualFolder("Op omroep", null);
-        for (int i = 0; i < omroepen.length; i++) {
-            final String url = SELECTIE_URL + "?searchitem=omroep&omroep=" + omroepen[i][0];
-            UitzendingFolder folder = new UitzendingFolder(omroepen[i][1], url, "items=actueel");
-            folder.addChild(new UitzendingFolder("Alle programma's", url));
-            omroepFolder.addChild(folder);
-        }
-
-        mainFolder.addChild(dezeweekFolder);
-        mainFolder.addChild(titelFolder);
-        mainFolder.addChild(genreFolder);
-        mainFolder.addChild(omroepFolder);
-        mainFolder.addChild(new Nederland24Folder());
-
-
-
-        //Voeg Uitzending Gemist toe aan alle configuraties
-        //TODO: Wachten op PMS om hook toe te voegen voor plugins om aan rootfolders iets toe te voegen
-        int i = 1;
-        RendererConfiguration conf = RendererConfiguration.getRendererConfiguration(i);
-        while (conf != RendererConfiguration.getDefaultConf()) {
-            conf.getRootFolder().addChild(mainFolder);
-
-            i++;
-            conf = RendererConfiguration.getRendererConfiguration(i);
-        }
-    }
-
-    public static void main(String args[]) {
-        //new UitzendingFolder("Woensdag", TOP50_URL).discoverChildren();
+        mainFolder.addChild(new Omroep());
+        //RTL 4
+        //RTL 5
+        mainFolder.addChild(new ProgrammaGemist("Net 5", ProgrammaGemist.NET5_URL));
+        mainFolder.addChild(new ProgrammaGemist("SBS 6", ProgrammaGemist.SBS6_URL));
+        //RTL 7
+        //RTL 8
+        mainFolder.addChild(new ProgrammaGemist("Veronica", ProgrammaGemist.VERONICA_URL));
+        return mainFolder;
     }
 
     public JComponent config() {
@@ -110,4 +39,5 @@ public class UitzendingGemist implements ExternalListener {
 
     public void shutdown() {
     }
+
 }
